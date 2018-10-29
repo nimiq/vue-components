d93efe4c1b4ebc64022eb3d1bae5f5b95c1d62c2
sisou <hello@soerenschwert.de>
2018-10-29 15:21:23 +0100

    Fix Vue/vue problem

--- vue.config.js
+++ vue.config.js
@@ -8,12 +8,4 @@ const configureWebpack = {
   }
 }
 
-if (process.argv.includes('build')) {
-  configureWebpack.externals = {
-    'vue': 'vue',
-    // 'vue-class-component': 'vue-class-component',
-    // 'vue-property-decorator': 'vue-property-decorator',
-  };
-}
-
 module.exports = { configureWebpack };
