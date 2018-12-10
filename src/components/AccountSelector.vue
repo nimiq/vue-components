c377c022e56d5c64282bcaaf16a80c9eef0d9c36
sisou <hello@soerenschwert.de>
2018-12-10 15:17:08 -0600

    Add login button to AccountSelector, add gradient background footer

--- src/components/AccountSelector.vue
+++ src/components/AccountSelector.vue
@@ -4,6 +4,9 @@
             <span class="nq-label">{{ walletLabel }}</span>
         </div>
         <AccountList :accounts="accounts" :walletId="walletId" :minBalance="minBalance" @account-selected="accountSelected"/>
+        <div class="footer">
+            <button v-if="showSwitchWallet" class="nq-button-s" @click="login">Login with another Wallet</button>
+        </div>
     </div>
 </template>
 
@@ -18,7 +21,7 @@
         @Prop(String) private walletLabel!: string;
         @Prop(Number) private walletType!: number;
         @Prop(Array) private accounts!: Array<{ label: string, userFriendlyAddress: string, balance?: number }>;
-        @Prop({type: Boolean, default: false}) private showSwitchWallet!: boolean;
+        @Prop({type: Boolean, default: true}) private showSwitchWallet!: boolean;
         @Prop(Number) private minBalance?: boolean;
 
         @Emit()
@@ -27,15 +30,7 @@
 
         @Emit()
         // tslint:disable-next-line no-empty
-        private switchWallet() {}
-
-        private get walletIconClass() {
-            switch (this.walletType) {
-                case 0: return 'keyguard';
-                case 1: return 'keyguard';
-                case 2: return 'ledger';
-            }
-        }
+        private login() {}
     }
 </script>
 
@@ -50,11 +45,49 @@
         margin: calc(1 * var(--nimiq-size, 8px)) 0;
         opacity: 0.7;
     }
+
+    .nq-button-s {
+        display: inline-block;
+        font-size: 1.75rem;
+        line-height: 2rem;
+        height: 3.375rem;
+        text-decoration: none;
+        font-weight: bold;
+        letter-spacing: 0.014em;
+        padding: 0 1.5rem;
+        background-color: rgba(31, 35, 72, 0.07); /* Based on Nimiq Blue */
+        color: var(--nimiq-blue);
+        border-radius: 1.6875rem;
+        transition: background-color 150ms;
+        will-change: background-color;
+        border: none;
+        cursor: pointer;
+        position: relative;
+        font-family: inherit;
+    }
+
+    .nq-button-s::after {
+        content: '';
+        display: block;
+        position: absolute;
+        left: -1rem;
+        top: -1rem;
+        right: -1rem;
+        bottom: -1rem;
+    }
+
+    .nq-button-s:hover,
+    .nq-button-s:active {
+        background-color: rgba(31, 35, 72, 0.12); /* Based on Nimiq Blue */
+    }
     /* END Nimiq Style */
 
     .account-selector {
         flex-grow: 1;
         overflow: auto;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
     }
 
     .wallet-label {
@@ -79,5 +112,19 @@
         flex-grow: 1;
         height: 1px;
         background: rgba(31, 35, 72, 0.1);
+    }
+
+    .footer {
+        padding: 4rem 0;
+        margin-top: -4rem;
+        text-align: center;
+        background: linear-gradient(transparent 0%, white 50%);
+        position: relative;
+        pointer-events: none;
+    }
+
+    .nq-button-s {
+        margin: auto;
+        pointer-events: all;
     }
 </style>
