7fa4bd3f137eba96e59eba852281a34fc5ff7f00
Don Maxito <maxharz@gmail.com>
2018-12-20 21:23:23 +0100

    if there is one address in the account, say '1 address' instead of '1 addresses'

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -4,7 +4,7 @@
         <div class="wallet-description" v-if="label">
             <div class="label">{{ label }}</div>
             <div class="details">
-                <span>{{numberAccounts}} Addresses</span>
+                <span>{{numberAccounts}} Address{{ numberAccounts > 0 && 'es' }}</span>
                 <Amount v-if="balance !== undefined" :amount="balance" :decimals="0"/>
             </div>
         </div>
