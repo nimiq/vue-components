63d659356bf277700dbf4e0ad79763fdcd1062f6
sisou <hello@soerenschwert.de>
2019-02-19 16:18:37 -0600

    Export new components

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 // Components
 export { default as Account } from './components/Account.vue';
+export { default as AccountInfo } from './components/AccountInfo.vue';
 export { default as AccountList } from './components/AccountList.vue';
 export { default as AccountSelector } from './components/AccountSelector.vue';
 export { default as Address } from './components/Address.vue';
+export { default as AddressDisplay } from './components/AddressDisplay.vue';
+export { default as AddressInput } from './components/AddressInput.vue';
 export { default as Amount } from './components/Amount.vue';
 export { default as AmountWithDetails } from './components/AmountWithDetails.vue';
 export { default as Contact } from './components/Contact.vue';
@@ -12,9 +15,9 @@ export { default as LabelInput } from './components/LabelInput.vue';
 export { default as LoadingSpinner } from './components/LoadingSpinner.vue';
 export { default as NewContact } from './components/NewContact.vue';
 export { default as OnboardingMenu } from './components/OnboardingMenu.vue';
-export { default as PageHeader } from './components/PageHeader.vue';
 export { default as PageBody } from './components/PageBody.vue';
 export { default as PageFooter } from './components/PageFooter.vue';
+export { default as PageHeader } from './components/PageHeader.vue';
 export { default as PaymentInfoLine } from './components/PaymentInfoLine.vue';
 export { default as SmallPage } from './components/SmallPage.vue';
 export { default as Wallet } from './components/Wallet.vue';
