d2c8bc7f2033aa4cd3bdf96f48604ed6d6662d10
Daniel <github.danimoh@neverbox.com>
2019-05-15 19:44:27 +0200

    Amount: support minDecimals and maxDecimals

--- src/components/Amount.vue
+++ src/components/Amount.vue
@@ -6,29 +6,50 @@
 </template>
 
 <script lang="ts">
-    import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-    @Component
-    export default class Amount extends Vue {
-        @Prop(Number) public amount!: number;
-        @Prop({type: Number, default: 2, validator(value) { return value >= 0 && value <= 5; }}) public decimals!: number;
-        @Prop({type: Boolean, default: false}) public showApprox!: boolean;
+@Component
+export default class Amount extends Vue {
+    // Amount in luna
+    @Prop(Number) public amount!: number;
+    // If set takes precedence over minDecimals and maxDecimals
+    @Prop({type: Number, validator: Amount._validateDecimals }) public decimals?: number;
+    @Prop({type: Number, default: 2, validator: Amount._validateDecimals }) public minDecimals!: number;
+    @Prop({type: Number, default: 5, validator: Amount._validateDecimals }) public maxDecimals!: number;
+    @Prop({type: Boolean, default: false}) public showApprox!: boolean;
 
-        private get formattedAmount() {
-            const roundingFactor = Math.pow(10, this.decimals);
-            const value = Math.floor((this.amount / 1e5) * roundingFactor) / roundingFactor;
-            const result = value.toFixed(this.decimals);
-            if (Math.abs(value) < 10000) return result;
-            // add thin spaces (U+202F) every 3 digits. Stop at the decimal separator if there is one
-            const regexp = this.decimals > 0 ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(\d{3})+$)/g;
-            return result.replace(regexp, '$1\u2009');
-        }
-
-        private get isApprox() {
-            const roundedNum = Number.parseFloat((this.amount / 1e5).toFixed(this.decimals));
-            return roundedNum.toFixed(5) !== (this.amount / 1e5).toFixed(5);
-        }
+    private static _validateDecimals(decimals) {
+        return decimals >= 0 && decimals <= 5 && Number.isInteger(decimals);
     }
+
+    private get formattedAmount() {
+        let minDecimals: number;
+        let maxDecimals: number;
+        if (typeof this.decimals === 'number') {
+            minDecimals = maxDecimals = this.decimals;
+        } else {
+            minDecimals = this.minDecimals;
+            maxDecimals = this.maxDecimals;
+        }
+
+        const roundingFactor = 10 ** maxDecimals;
+        const value = Math.floor((this.amount / 1e5) * roundingFactor) / roundingFactor;
+
+        const result = parseFloat(value.toFixed(minDecimals)) === value
+            ? value.toFixed(minDecimals)
+            : value.toString();
+
+        if (Math.abs(value) < 10000) return result;
+        // add thin spaces (U+202F) every 3 digits. Stop at the decimal separator if there is one
+        const regexp = minDecimals > 0 ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(\d{3})+$)/g;
+        return result.replace(regexp, '$1\u202F');
+    }
+
+    private get isApprox() {
+        const roundedNum = Number.parseFloat((this.amount / 1e5).toFixed(this.decimals));
+        return roundedNum.toFixed(5) !== (this.amount / 1e5).toFixed(5);
+    }
+}
 </script>
 
 <style scoped>
