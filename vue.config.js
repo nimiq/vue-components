ee3581582f3d4adef7f73399c2c85c701691fe5b
Don Maxito <maxharz@gmail.com>
2019-05-19 12:07:03 +0200

    disable lazy loading for qr-code

--- vue.config.js
+++ vue.config.js
@@ -7,11 +7,6 @@ const configureWebpack = {
 }
 
 if (process.argv.includes('build')) {
-  configureWebpack.externals = {
-    'vue': 'vue',
-    'vue-class-component': 'vue-class-component',
-    'vue-property-decorator': 'vue-property-decorator',
-  };
   configureWebpack.mode = 'production';
   configureWebpack.optimization = {
     providedExports: true,
