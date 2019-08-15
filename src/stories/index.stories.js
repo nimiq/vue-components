a3c890aa3757cee11db346b3cf526a5e14871e4d
Sebastian <sebastian@nimiq.com>
2019-08-15 12:31:07 -0600

    update Tooltip

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -192,10 +192,13 @@ storiesOf('Basic', module)
         }
     })
     .add('Tooltip', () => {
+        const fontSize = number('Font size (rem)', 3);
         return {
             data() {
                 return {
                     refsLoaded: false,
+                    message: '',
+                    fontSize,
                 };
             },
             computed: {
@@ -204,20 +207,30 @@ storiesOf('Basic', module)
                         return this.$refs.target;
                     else return null;
                 },
+                style() {
+                    return {
+                        fontSize: this.fontSize + 'rem',
+                    };
+                }
             },
             mounted() {
                 this.refsLoaded = true;
             },
-            components: { SmallPage, PageHeader, PageBody, Tooltip, Icons, Account },
+            components: { SmallPage, PageHeader, PageBody, Tooltip, Icons, Account, LabelInput },
             template: windowTemplate`<SmallPage>
                             <PageHeader>Test</PageHeader>
-                            <PageBody style="overflow-y: scroll;" ref="target">
-                                <div style="height:300px">&nbsp;</div>
-                                Look and <Tooltip :reference="target">
-                                    This is the Tooltip I was talking about.
-                                    <Account address="NQ55 VDTM 6PVTN672 SECN JKVD 9KE4 SD91 PCCM" />
-                                </Tooltip>
-                                <div style="height:3000px">&nbsp;</div>
+                            <PageBody style="overflow-y: scroll; position:relative;" ref="target">
+                                <div style="height:300px"></div>
+                                <div style="max-width: 100%; display: flex; align-items: center;">
+                                    <LabelInput v-model="message" style="display: inline;"/>
+                                    <Tooltip :reference="target" :calculation-trigger="message" :style="style">
+                                        <div style="font-size: 2rem;">
+                                            This is the Tooltip I was talking about.
+                                            <Account address="NQ55 VDTM 6PVTN672 SECN JKVD 9KE4 SD91 PCCM" />
+                                        </div>
+                                    </Tooltip>
+                                </div>
+                                <div style="height:3000px"></div>
                             </PageBody>
                         </SmallPage>`,
         };
