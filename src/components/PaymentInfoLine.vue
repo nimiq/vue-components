baf058630a0bac2978e049aa27543bb254cb418b
Daniel <github.danimoh@neverbox.com>
2019-09-16 14:28:39 +0200

    PaymentInfoLine: expand api for new checkout
    - support arbitrary crypto currencies
    - support fiat currencies
    - add Timer
    - small positioning adaptions and style fixes
    
    Co-authored-by: Sebastian <sebastian@nimiq.com>

--- src/components/PaymentInfoLine.vue
+++ src/components/PaymentInfoLine.vue
@@ -1,36 +1,76 @@
 <template>
     <div class="info-line">
-        <Amount :amount="amount + fee" :decimals="decimals" />
+        <div class="amounts">
+            <Amount class="nq-light-blue"
+                :currency="cryptoAmount.currency"
+                :amount="cryptoAmount.amount"
+                :totalDecimals="cryptoAmount.decimals"
+                :minDecimals="0"
+                :maxDecimals="Math.min(4, cryptoAmount.decimals)"
+            />
+            <Amount v-if="fiatAmount" class="fiat-amount nq-blue"
+                :currency="fiatAmount.currency"
+                :amount="fiatAmount.amount"
+                :totalDecimals="fiatAmount.decimals"
+                :decimals="fiatAmount.decimals"
+            />
+        </div>
         <div class="arrow-runway">
             <ArrowRightSmallIcon/>
         </div>
         <a href="javascript:void(0)" class="description" @click="merchantInfoClicked">
             <Account :address="address" :image="shopLogoUrl" :label="originDomain" />
-            <div class="info-circle-container">
+            <div v-if="!startTime || !endTime" class="info-circle-container">
                 <InfoCircleIcon class="info-circle"/>
             </div>
         </a>
+        <Timer v-if="startTime && endTime" ref="timer" :startTime="startTime" :endTime="endTime" />
     </div>
 </template>
 
 <script lang="ts">
-import {Component, Prop, Emit, Vue} from 'vue-property-decorator';
-import Amount from './Amount.vue';
+// this imports only the type without bundling the library
+type BigInteger = import ('big-integer').BigInteger;
+
+import { Component, Prop, Emit, Vue } from 'vue-property-decorator';
 import Account from './Account.vue';
+import Timer from './Timer.vue';
+import Amount from './Amount.vue';
 import { InfoCircleIcon, ArrowRightSmallIcon } from './Icons';
 
-@Component({components: {Amount, Account, InfoCircleIcon, ArrowRightSmallIcon}})
-export default class PaymentInfoLine extends Vue {
+interface AmountInfo {
+    amount: number | bigint | BigInteger; // in the smallest unit
+    currency: string;
+    decimals: number;
+}
 
+function amountInfoValidator(value: any) {
+    return 'amount' in value && 'currency' in value && 'decimals' in value
+        && (typeof value.amount === 'number' || typeof value.amount === 'bigint'
+            || (value.amount && value.amount.constructor && value.amount.constructor.name.endsWith('Integer')))
+        && typeof value.currency === 'string'
+        && typeof value.decimals === 'number' && Number.isInteger(value.decimals);
+}
+
+@Component({components: {Account, Timer, Amount, InfoCircleIcon, ArrowRightSmallIcon}})
+export default class PaymentInfoLine extends Vue {
     private get originDomain() {
         return this.origin.split('://')[1];
     }
-    @Prop({type: Number, default: 2}) public decimals!: number;
-    @Prop(Number) private amount!: number;
-    @Prop(Number) private fee!: number;
-    @Prop(String) private origin!: string;
-    @Prop(String) private address?: string;
-    @Prop(String) private shopLogoUrl?: string;
+
+    @Prop({type: Object, validator: amountInfoValidator}) public cryptoAmount!: AmountInfo;
+    @Prop({type: Object, validator: amountInfoValidator}) public fiatAmount?: AmountInfo;
+    @Prop(String) public origin!: string;
+    @Prop(String) public address?: string;
+    @Prop(String) public shopLogoUrl?: string;
+    @Prop(Number) public startTime?: number;
+    @Prop(Number) public endTime?: number;
+
+    public async setTime(time: number) {
+        await this.$nextTick(); // let vue update in case the timer was just added
+        if (!this.$refs.timer) return;
+        (this.$refs.timer as Timer).synchronize(time);
+    }
 
     @Emit()
     // tslint:disable-next-line no-empty
@@ -44,20 +84,35 @@ export default class PaymentInfoLine extends Vue {
         flex-direction: row;
         justify-content: space-between;
         box-sizing: border-box;
-        margin: 2rem 2.5rem 1rem 2.5rem;
+        margin: 1.75rem 2.5rem 1rem 2.375rem;
         flex-shrink: 0;
         font-size: 2rem;
         line-height: 1.5;
         font-weight: normal;
     }
 
+    .amounts {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-bottom: .125rem;
+    }
+
     .amount {
         font-weight: bold;
-        color: var(--nimiq-light-blue);
+    }
+
+    .fiat-amount {
+        margin-top: .25rem;
+        font-size: 1.625rem;
+        line-height: 1;
+        font-weight: 600;
+        opacity: .6;
     }
 
     .arrow-runway {
         flex-grow: 1;
+        min-width: 3rem;
         display: flex;
         flex-direction: row;
         align-items: center;
@@ -97,23 +152,26 @@ export default class PaymentInfoLine extends Vue {
     .account {
         padding: 0;
         width: auto !important;
+        flex-shrink: 1;
     }
 
     .account >>> .identicon {
         min-width: unset;
         width: 3.375rem;
         height: 3.375rem;
-        margin-top: -0.25rem;
-        margin-bottom: -0.125rem;
-        margin-right: 1rem;
+        margin-right: 0;
     }
 
     .account >>> .label {
-        padding-left: 0;
+        padding-left: .75rem;
+        margin-bottom: .25rem;
         transition: opacity .3s ease;
         font-weight: unset;
-        opacity: 1;
-        mask-image: unset; /* Remove gradient-fade-out */
+        opacity: 1 !important;
+        /* Remove gradient-fade-out and use text-overflow instead */
+        mask-image: unset;
+        white-space: nowrap;
+        text-overflow: fade;
     }
 
     .info-circle-container {
@@ -153,4 +211,8 @@ export default class PaymentInfoLine extends Vue {
         opacity: 1;
     }
 
+    .timer {
+        margin: auto -.5rem auto 1rem;
+        flex-shrink: 0;
+    }
 </style>
