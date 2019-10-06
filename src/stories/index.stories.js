0d50c5c33cfa4b3dce6f69557638042a8b7399c0
Daniel <github.danimoh@neverbox.com>
2019-10-07 01:36:26 +0200

    CopyableField: add stories and export in main

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -15,6 +15,7 @@ import AmountWithDetails from '../components/AmountWithDetails.vue';
 import CircleSpinner from '../components/CircleSpinner.vue';
 import Contact from '../components/Contact.vue';
 import ContactList from '../components/ContactList.vue';
+import CopyableField from '../components/CopyableField.vue';
 import FiatAmount from '../components/FiatAmount.vue';
 import Identicon from '../components/Identicon.vue';
 import LabelInput from '../components/LabelInput.vue';
@@ -500,6 +501,25 @@ storiesOf('Components', module)
                     <button class="nq-button" @click="reset">Reset</button>
                 </div>
             `
+        };
+    })
+    .add('CopyableField', () => {
+        const label = text('label', '');
+        const values = object('values', {
+            a: 'Value a',
+            b: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+        });
+
+        return {
+            components: {CopyableField},
+            data: () => ({ label, values }),
+            template: `
+                <CopyableField
+                    :label="label"
+                    :values="values"
+                    style="color: white; background: var(--nimiq-blue-bg); width: 30rem"
+                />
+            `,
         };
     })
     .add('Wallet', () => {
