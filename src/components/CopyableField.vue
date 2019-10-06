eddffb9402d99cd06cdbf94fc0c4f53a1d774057
Daniel <github.danimoh@neverbox.com>
2019-10-07 00:50:52 +0200

    CopyableField: several improvements
    - make props reactive to updates
    - use vue prop validation instead of manual validation
    - resize text according to actually available space
    - make hasSingleValue a getter
    - make button idle background transparent and add margins around
      buttons
    - some code cleanup

--- src/components/CopyableField.vue
+++ src/components/CopyableField.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="copyable-field-wrapper">
+    <div class="copyable-field">
         <span class="nq-label" v-if="label">{{label}}</span>
-        <div class="copyable-field"
-            :class="{ 'single-value': hasSingleValue, copied }"
-            @click="copy(values[currentKey])">
-            <span :style="{ fontSize: dynamicFontSize+'rem' }">
-                {{values[currentKey]}}
-            </span>
+        <div class="copyable-field-content" :class="{ 'single-value': hasSingleValue, copied }" @click="copy">
+            <div ref="value-container" class="value-container" :style="{ fontSize: fontSize+'rem' }">
+                <span ref="value" class="value">
+                    {{values[currentKey]}}
+                </span>
+            </div>
             <button
                 class="nq-button-s"
-                v-if="values && Object.keys(values).length > 1"
-                v-for="(value, key) in values"
-                @click.stop="changeKey(key)"
+                v-if="!hasSingleValue"
+                v-for="key in Object.keys(values)"
+                @click.stop="currentKey = key"
                 :class="{ inverse: currentKey === key }"
             >{{key}}</button>
         </div>
@@ -19,70 +19,67 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 import { Clipboard } from '@nimiq/utils';
-
-interface CopyableFieldValues {
-    [ k: string ]: string | number;
-}
-
-enum InitialValue {
-    FONT_SIZE = 3,
-    MAX_LENGTH = 21,
-}
 
 @Component
 export default class CopyableField extends Vue {
+    private static readonly DEFAULT_FONT_SIZE = 3; // in rem
+
     @Prop({
         required: true,
         type: Object,
+        validator: (values: object) => typeof values === 'object' && Object.keys(values).length > 0,
     })
-    public values!: CopyableFieldValues;
+    public values!: { [key: string]: any };
 
     @Prop(String)
     public label?: string;
 
-    private currentKey: string | number = '';
-    private hasSingleValue: boolean = false;
-    private dynamicFontSize: number = InitialValue.FONT_SIZE;
+    private currentKey: string = '';
+    private fontSize: number = CopyableField.DEFAULT_FONT_SIZE;
     private copied: boolean = false;
     private _copiedResetTimeout: number | undefined;
 
     private mounted() {
-        const keys: string[] = Object.keys(this.values);
-
-        if (!this.values || keys.length === 0 || keys.length > 2) {
-            throw new Error('CopyableField: values prop should have between 1 and 2 entries');
-        }
-
-        if (keys.length === 1) {
-            this.hasSingleValue = true;
-        }
-
-        this.changeKey(keys[0]);
+        this._updateFontSize = this._updateFontSize.bind(this);
+        window.addEventListener('resize', this._updateFontSize);
+        this._updateFontSize();
     }
 
-    private changeKey(key: string | number) {
-        this.currentKey = key;
-        this._updateFontSize(key);
+    private destroyed() {
+        window.removeEventListener('resize', this._updateFontSize);
     }
 
-    private _updateFontSize(key: string | number) {
-        const valueLength = (this.values[key] + '').length;
-        const keysLength = Object.keys(this.values).join('').length;
-        const currentMaxLength = InitialValue.MAX_LENGTH - keysLength;
+    private get hasSingleValue() {
+        return Object.keys(this.values).length === 1;
+    }
 
-        if (valueLength > currentMaxLength && !this.hasSingleValue) {
-            this.dynamicFontSize = (InitialValue.FONT_SIZE / valueLength) * currentMaxLength;
-        } else if (valueLength > currentMaxLength && this.hasSingleValue) {
-            this.dynamicFontSize = (InitialValue.FONT_SIZE / valueLength) * (InitialValue.MAX_LENGTH + 1);
+    @Watch('values', { immediate: true })
+    private _onValuesChange() {
+        const keys = Object.keys(this.values);
+        if (!this.currentKey || !keys.includes(this.currentKey)) {
+            this.currentKey = keys[0]; // will also trigger _updateFontSize
         } else {
-            this.dynamicFontSize = InitialValue.FONT_SIZE;
+            this._updateFontSize(); // trigger manually
         }
     }
 
-    private copy(value: string | number) {
-        Clipboard.copy(value + '');
+    @Watch('currentKey')
+    private async _updateFontSize() {
+        await Vue.nextTick(); // let Vue render the component first
+        const valueContainer = this.$refs['value-container'] as HTMLDivElement;
+        const valueElement = this.$refs.value as HTMLSpanElement;
+        valueElement.style.fontSize = `${CopyableField.DEFAULT_FONT_SIZE}rem`;
+        const availableWidth = valueContainer.offsetWidth;
+        const referenceWidth = valueElement.offsetWidth;
+        const scaleFactor =  availableWidth / referenceWidth;
+        valueElement.style.fontSize = '';
+        this.fontSize = Math.min(CopyableField.DEFAULT_FONT_SIZE, CopyableField.DEFAULT_FONT_SIZE * scaleFactor);
+    }
+
+    private copy() {
+        Clipboard.copy(this.values[this.currentKey].toString());
         this.copied = true;
 
         window.clearTimeout(this._copiedResetTimeout);
@@ -94,39 +91,38 @@ export default class CopyableField extends Vue {
 </script>
 
 <style scoped>
-    .copyable-field,
-    .copyable-field button,
-    .copyable-field::after,
-    .copyable-field.single-value span {
+    .copyable-field-content,
+    .copyable-field-content::after,
+    button,
+    .single-value .value-container {
         transition-duration: .25s;
         transition-timing-function: var(--nimiq-ease);
     }
 
-    .copyable-field-wrapper,
-    .copyable-field {
+    .copyable-field,
+    .copyable-field-content {
         display: flex;
         width: 100%;
         align-items: center;
     }
 
-    .copyable-field-wrapper {
+    .copyable-field {
         flex-direction: column;
         justify-content: space-between;
+        color: white;
     }
 
-    .copyable-field {
-        flex-direction: row;
+    .copyable-field-content {
         height: 6.25rem;
         position: relative;
         border-radius: 0.5rem;
         line-height: 6.25rem;
-        font-size: 3rem;
         cursor: pointer;
         background: rgba(255, 255, 255, 0.1);
         transition-property: background;
     }
 
-    .copyable-field::after {
+    .copyable-field-content::after {
         content: 'COPIED';
         position: absolute;
         right: 0;
@@ -134,44 +130,64 @@ export default class CopyableField extends Vue {
         transform: translateY(-50%);
         font-size: 1.75rem;
         color: var(--nimiq-light-blue);
+        pointer-events: none;
         opacity: 0;
-        visibility: hidden;
-        transition-property: opacity, visibility;
+        transition-property: opacity;
     }
 
-    .copyable-field,
-    .copyable-field::after {
+    .copyable-field-content,
+    .copyable-field-content::after {
         padding: 0 3rem;
     }
 
-    .copyable-field:hover {
+    .copyable-field-content:hover {
         background: rgba(255, 255, 255, 0.16);
     }
 
-    .copyable-field.copied::after {
+    .copyable-field-content.copied::after {
         opacity: 1;
-        visibility: visible;
     }
 
-    .copyable-field button {
+    button {
+        margin-left: .5rem;
         transition-property: background, color, opacity;
     }
 
-    .copyable-field button.inverse {
+    button:first-of-type {
+        margin-left: 2.5rem;
+    }
+
+    button:not(.inverse) {
+        background: transparent;
+    }
+
+    button:not(.inverse):hover {
+        background: rgba(255, 255, 255, .07);
+    }
+
+    button.inverse {
         color: white;
     }
 
-    .copyable-field.copied button {
+    .copied button {
         opacity: 0;
     }
 
-    .copyable-field.single-value span {
+    .value-container {
+        display: flex;
+        align-items: center;
+        flex-grow: 1;
+        overflow-x: hidden; /* avoid overflow of the value while it's not resized yet */
+        white-space: nowrap;
+    }
+
+    .single-value .value-container {
         mask-image: linear-gradient(90deg, black 60%, transparent 80%);
-        mask-size: 150%;
+        mask-size: 160%;
         transition-property: mask-size;
     }
 
-    .copyable-field.single-value.copied span {
+    .single-value.copied .value-container {
         mask-size: 100%;
     }
 
@@ -180,16 +196,8 @@ export default class CopyableField extends Vue {
         color: rgba(255, 255, 255, 0.5);
     }
 
-    .nq-button-s {
-        text-transform: uppercase;
-    }
-
     .nq-label {
         margin-top: 3rem;
         margin-bottom: 2rem;
-    }
-
-    span {
-        flex-grow: 1;
     }
 </style>
