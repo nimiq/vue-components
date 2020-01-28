20b6bd9e97599aecc38b0e0ce6b27caafac251a3
sisou <hello@soerenschwert.de>
2020-01-28 15:23:21 -0600

    Update Storybook to v5

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../src/stories/*.stories.(js|ts|mdx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    '@storybook/addon-notes',
+    '@storybook/addon-viewport'
+  ]
+}
