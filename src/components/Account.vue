nimiq/vue-components

commit 238af066dd0f475efeda41ddaf901cd409c0ba47

Allow Account to display as Cashlink

src/components/Account.vue
@@ -1,7 +1,15 @@
 <template>
-    <div class="account" :class="[{ editable }, layout]">
+    <div class="account" :class="[{ editable }, layout, {cashlink: displayAsCashlink}]">
         <div class="identicon-and-label">
-            <img v-if="showImage" class="identicon account-image" :src="image" @error="showImage = false">
+            <div v-if="showImage" class="identicon">
+                <img class="account-image" :src="image" @error="showImage = false">
+                <div class="outline"></div>
+            </div>
+            <div class="identicon" div v-else-if="displayAsCashlink">
+                <div class="nq-blue-bg">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="none" stroke="white" stroke-linecap="round" stroke-width="2.5"><path d="M40.25 23.25v-.5a6.5 6.5 0 0 0-6.5-6.5h-3.5a6.5 6.5 0 0 0-6.5 6.5v6.5a6.5 6.5 0 0 0 6.5 6.5h2"/><path d="M23.75 40.75v.5a6.5 6.5 0 0 0 6.5 6.5h3.5a6.5 6.5 0 0 0 6.5-6.5v-6.5a6.5 6.5 0 0 0-6.5-6.5h-2"/><path d="M32 11.25v4M32 48.75v4"/></svg>
+                </div>
+            </div>
             <Identicon v-else :address="address"/>
 
             <div v-if="!editable" class="label" :class="{ 'address-font': _isLabelAddress }">{{ label }}</div>
@@ -27,6 +35,7 @@
     export default class Account extends Vue {
         @Prop(String) public address!: string;
         @Prop(String) public image?: string;
+        @Prop({type: Boolean, default: false}) public displayAsCashlink!: boolean;
         @Prop(String) public label!: string;
         @Prop(String) public placeholder?: string;
         @Prop(String) public walletLabel?: string;
@@ -117,8 +126,31 @@
         margin-bottom: 1.25rem;
     }
 
-    .account-image {
-        border-radius: 1rem;
+    .cashlink .identicon {
+        padding: .5rem;
+    }
+
+    .cashlink .identicon div {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+    }
+    .cashlink .identicon:before {
+        display: block;
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        border: .25rem solid rgba(31, 35, 72, .2); /* based on nimiq-blue */
+    }
+
+    .identicon .account-image {
+        width: 100%;
+        height: 100%;
+        clip-path: url(#nimiq-hexagon-clip);
     }
 
     .wallet-label {

src/components/AmountInput.vue
@@ -129,9 +129,6 @@ export default class AmountInput extends Vue {
         justify-content: center;
         width: 100%;
         font-size: 8rem;
-        border-top: .125rem solid var(--nimiq-highlight-bg);
-        margin-top: 1rem;
-        padding-top: 2rem;
     }
 
     .amount form {

src/components/SendTx.vue
@@ -317,6 +317,9 @@ enum Details {
         display: flex;
         align-items: baseline;
         height: 14.5rem; /* 12.5rem height + 2rem padding */
+        border-top: .125rem solid var(--nimiq-highlight-bg);
+        margin-top: 1rem;
+        padding-top: 2rem;
     }
 
     .address-input {

src/stories/index.stories.js
@@ -176,15 +176,17 @@ storiesOf('Components', module)
         }, '');
         const balance = number('Balance (can be empty)', 12023110);
         const editable = boolean('Editable', false);
+        const cashlink = boolean('Cashlink', false);
 
         return {
             components: {Account},
             methods: {
                 changed: action('changed'),
             },
-            data: () => ({ layout, address, label, walletLabel, image, balance, editable }),
+            data: () => ({ layout, address, label, walletLabel, image, balance, editable, cashlink }),
             template: `<Account :layout="layout" :address="address" :label="label" :walletLabel="walletLabel"
-                :image="image" :balance="balance" :editable="editable" @changed="changed"></Account>`,
+                :image="image" :balance="balance" :editable="editable" :displayAsCashlink="cashlink"
+                @changed="changed"/>`,
         };
     })
     .add('AccountList', () => {

commit 616940a824fb07c076e9cf641991ac5f22b1d6e1

Account: fix account image clipping
Fix account image clipping broken by 238af06 which was initially
introduced by cb2502d (#50)

src/components/Account.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="account" :class="[{ editable }, layout, {cashlink: displayAsCashlink}]">
         <div class="identicon-and-label">
-            <div v-if="showImage" class="identicon">
-                <img class="account-image" :src="image" @error="showImage = false">
-                <div class="outline"></div>
-            </div>
+            <img v-if="showImage" class="identicon account-image" :src="image" @error="showImage = false">
             <div v-else-if="displayAsCashlink" class="identicon">
                 <div class="nq-blue-bg">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="none" stroke="white" stroke-linecap="round" stroke-width="2.5"><path d="M40.25 23.25v-.5a6.5 6.5 0 0 0-6.5-6.5h-3.5a6.5 6.5 0 0 0-6.5 6.5v6.5a6.5 6.5 0 0 0 6.5 6.5h2"/><path d="M23.75 40.75v.5a6.5 6.5 0 0 0 6.5 6.5h3.5a6.5 6.5 0 0 0 6.5-6.5v-6.5a6.5 6.5 0 0 0-6.5-6.5h-2"/><path d="M32 11.25v4M32 48.75v4"/></svg>
@@ -151,10 +148,8 @@
         border: .25rem solid rgba(31, 35, 72, .2); /* based on nimiq-blue */
     }
 
-    .identicon .account-image {
-        width: 100%;
-        height: 100%;
-        clip-path: url(#nimiq-hexagon-clip);
+    .account-image {
+        border-radius: 1rem;
     }
 
     .wallet-label {