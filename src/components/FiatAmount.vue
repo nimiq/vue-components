7fe3046f2f172a11e16ae21c64fe7dec0919adbb
Daniel <github.danimoh@neverbox.com>
2020-04-13 12:20:31 +0200

    FiatAmount: simplify maxRelativeDeviation logic
    It now also renders numbers as short as possible, at the expense
    of slightly higher computational cost. Typical amounts that are
    not small fractions can however be rendered immediately as before.

--- src/components/FiatAmount.vue
+++ src/components/FiatAmount.vue
@@ -6,10 +6,13 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { FormattableNumber, toNonScientificNumberString } from '@nimiq/utils';
+import { FormattableNumber } from '@nimiq/utils';
 
 @Component
 export default class FiatAmount extends Vue {
+    private static readonly NUMBER_REGEX = /(-)?\D*(\d+)(\.(\d+))?/;
+    private static readonly DECIMAL_SEPARATOR_REGEX = /(\d)\D(\d)/;
+
     @Prop({
         type: Number,
         required: true,
@@ -41,48 +44,30 @@ export default class FiatAmount extends Vue {
             currency: this.currency,
             currencyDisplay: 'symbol',
             useGrouping: false,
-            minimumFractionDigits: undefined,
+            minimumFractionDigits: undefined, // start with decimal count typical for this.currency, e.g. 2 for eur
         };
         let formatted: string;
-        formatted = this.amount.toLocaleString([localeWithLatinNumbers, 'en-US'], formattingOptions)
-            // Enforce a dot as decimal separator for consistency. Using capturing groups instead of
-            // lookahead/lookbehind to avoid browser support limitations.
-            .replace(/(\d)\D(\d)/, '$1.$2');
-        const numberRegex = /(-)?\D*(\d+)(\.(\d+))?/;
-        const [, sign, integers, decimalsIncludingSeparator, defaultDecimals] = formatted.match(numberRegex);
-        const formattedNumber = `${sign || ''}${integers}${decimalsIncludingSeparator || ''}`;
-        const relativeDeviation = Math.abs((this.amount - Number.parseFloat(formattedNumber)) / this.amount);
+        let integers: string;
+        let relativeDeviation: number;
 
-        if (relativeDeviation > this.maxRelativeDeviation) {
-            // determine how many decimals we have to use
-            let sampleString: string;
-            if (this.maxRelativeDeviation === 0) {
-                // have to use all decimals of the original number, therefore we measure the decimals from this.amount.
-                sampleString = toNonScientificNumberString(this.amount);
-            } else {
-                // Check which decimal changes when advancing in maxAllowedDeviation steps.
-                // Note that this might in some cases generate one digit more than required for the requested precision:
-                // For example 1.011 JPY for max error 0.001 will be rendered as 1.011 while 1.01 would be acceptable.
-                // This happens if the last required digit is <= the first significant digit.
-                const maxAllowedDeviation = this.maxRelativeDeviation * this.amount;
-                sampleString = maxAllowedDeviation.toLocaleString('en-US', { // note: renders as non-scientific number
-                    maximumSignificantDigits: 1,
-                });
-            }
-            const [, , , , sampleDecimals] = sampleString.match(numberRegex);
-            formattingOptions.minimumFractionDigits = Math.min(
-                20, // maximum allowed by toLocaleString
-                Math.max(
-                    sampleDecimals ? sampleDecimals.length : 0,
-                    defaultDecimals ? defaultDecimals.length : 0,
-                ),
-            );
-
+        do {
             formatted = this.amount.toLocaleString([localeWithLatinNumbers, 'en-US'], formattingOptions)
-                .replace(/(\d)\D(\d)/, '$1.$2');
-        }
+                // Enforce a dot as decimal separator for consistency and parseFloat. Using capturing groups instead of
+                // lookahead/lookbehind to avoid browser support limitations.
+                .replace(FiatAmount.DECIMAL_SEPARATOR_REGEX, '$1.$2');
+            const regexMatch = formatted.match(FiatAmount.NUMBER_REGEX)!;
+            const [/* full match */, sign, /* integers */, decimalsIncludingSeparator, decimals] = regexMatch;
+            integers = regexMatch[2];
+            const formattedNumber = `${sign || ''}${integers}${decimalsIncludingSeparator || ''}`;
+            relativeDeviation = Math.abs((this.amount - Number.parseFloat(formattedNumber)) / this.amount);
+
+            formattingOptions.minimumFractionDigits = decimals ? decimals.length + 1 : 1;
+        } while (relativeDeviation > this.maxRelativeDeviation
+            && formattingOptions.minimumFractionDigits <= 20 // maximum allowed value for minimumFractionDigits
+        );
 
         // apply integer grouping
+        if (integers.length <= 4) return formatted;
         return formatted.replace(integers, new FormattableNumber(integers).toString(true));
     }
 }
